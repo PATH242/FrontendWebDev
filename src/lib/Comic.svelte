476229eb3a6274091af46ca3f29de5bc69e31aed
Menna Awadallah <mennashaheen2357@gmail.com>
2023-03-15 21:22:02 +0300

    requirement for modifications

--- src/lib/Comic.svelte
+++ src/lib/Comic.svelte
@@ -1,4 +1,7 @@
 <div id='joke'>
+    <h3>{image_title}</h3>
+    <img src={image_url} alt={image_alt}>
+    <h4>{comic_date_ar} {comic_date_eng}</h4>
 </div>
 <script lang="ts">
     import type { comicAPI } from './comic.ts';
@@ -11,15 +14,19 @@
         console.log(comic_id)
         return comic_id;
     }
+    let image_title : string = "Loading comic";
+    let image_url : string = "../assets/loading.jpg";
+    let image_alt : string = "loading";
+    let comic_date_ar : string, comic_date_eng : string;
     // Get image, its name, its alt text, and its date from XKCD API using fetch.
     async function getComic(comic_id : number){
         var comic_url : string = 'https://getxkcd.vercel.app/api/comic?num=' + comic_id;
         let response : Response = await fetch(comic_url);
         let responseData  : comicAPI = await response.json();
-        const image_title : string = responseData.title;
-        const image_url : string = responseData.img;
-        const image_alt : string = responseData.alt;
-        const image_year : number = parseInt(responseData.year), image_month : number = parseInt(responseData.month), image_day : number = parseInt(responseData.day);
+        image_title = responseData.title;
+        image_url = responseData.img;
+        image_alt = responseData.alt;
+        const image_year : number = parseInt(responseData.year), image_month : number = parseInt(responseData.month), image_day = parseInt(responseData.day);
         const image_date : Date = new Date(image_year, image_month -1, image_day);
         const options  = {
             weekday : "long" as const,
@@ -27,35 +34,9 @@
             month : "long" as const,
             day : "numeric" as const
         }
-        const comic_date_ar : string = image_date.toLocaleDateString("ar-EG", options);
-        const comic_date_eng : string = image_date.toLocaleDateString("en-US", options);
-        
-        
-        const comic_h3 = document.createElement('h3');
-        const comic_title = document.createTextNode(image_title);
-        document.getElementById('joke')?.appendChild(comic_title);
-        comic_h3.appendChild(comic_title);
-        document.getElementById('joke')?.appendChild(comic_h3);
-
-        const joke_element = document.getElementById('joke');
-        const comic = document.createElement('img');
-        comic.src = image_url;
-        comic.alt = image_alt;
-        if(joke_element != null)
-            joke_element.appendChild(comic);
-        
-    const comic_h4 = document.createElement('text');
-    const comic_alt = document.createTextNode(image_alt);
-    comic_h4.appendChild(comic_alt);
-    if(joke_element != null)
-            joke_element.appendChild(comic_h4);
-
-    const comic_h5 = document.createElement('text');
-    const comic_date = document.createTextNode(comic_date_ar + " " + comic_date_eng); 
-    comic_h5.appendChild(comic_date);
-    if(joke_element != null)
-            joke_element.appendChild(comic_h5);
-    return;
+        comic_date_ar = image_date.toLocaleDateString("ar-EG", options);
+        comic_date_eng = image_date.toLocaleDateString("en-US", options);
+        return;
     }
 
     getComicID().then(comic_id => processComicID(comic_id)).catch(err => console.log(err.message));
