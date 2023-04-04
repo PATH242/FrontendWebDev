cfcc129dbe5b0b852b15b6b62c6b5df8a9ac08e0
PATH242 <mennashaheen2357@gmail.com>
2023-04-05 02:08:17 +0300

    remove additional code

--- src/main.ts
+++ src/main.ts
@@ -1,21 +1,8 @@
 import './app.css'
 import App from './App.svelte'
-import ghpages from "gh-pages"
 
-ghpages.publish(
-    'public', // path to public directory
-    {
-        branch: 'main',
-        repo: 'https://github.com/username/yourproject.git', // Update to point to your repository  
-        user: {
-            name: 'PATH242', 
-            email: 'mennashaheen2357@gmail.com'
-        }
-    },
-    () => {
-    const app = new App({
-      target: document.getElementById('app'),
-    })
-  })
+const app = new App({
+  target: document.getElementById('app'),
+})
 
-    export default app
+export default app
