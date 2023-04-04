1528fe0c82373d16f0b649ca6b1cbc5a2941abd3
Menna Awadallah <52926455+PATH242@users.noreply.github.com>
2023-04-04 17:02:17 +0300

    Delete build directory

--- build/_app/immutable/chunks/1.99e0f780.js
+++
@@ -1,1 +0,0 @@
-import{default as t}from"../entry/error.svelte.866c7903.js";export{t as component};
--- build/_app/immutable/chunks/3.00702b73.js
+++
@@ -1,1 +0,0 @@
-import{default as t}from"../entry/about-page.svelte.43829586.js";export{t as component};
